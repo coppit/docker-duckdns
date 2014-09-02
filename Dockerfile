0bd34e53936ed306d5dcdf5ae42c57c3b77764dd
David Coppit <david@coppit.org>
2014-09-02 08:13:52 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# The config file is named duck.conf, and has the format:
+# DOMAINS=<your domain>
+# TOKEN=<your token>
+# INVERVAL=<update period. e.g. "5m" or "1h">
+
+# Output will be written to /config/duck.log
+
+FROM phusion/baseimage:0.9.11
+
+MAINTAINER coppit
+
+VOLUME ["/config"]
+
+ADD duck.sh /root/duckdns/duck.sh
+
+CMD /bin/bash -c '/root/duckdns/duck.sh >>/config/duck.log 2>&1'
---
+++ duck.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while true
+do
+  if [ ! -f /config/duck.conf ]; then
+    echo "Could not find /config/duck.conf"
+    exit 1
+  fi
+
+  . /config/duck.conf
+
+  if [ -z "$INTERVAL" ]; then
+    echo "INTERVAL must be defined in duck.conf"
+    exit 1
+  fi
+
+  if [ -z "$DOMAINS" ]; then
+    echo "DOMAINS must be defined in duck.conf"
+    exit 1
+  fi
+
+  if [ -z "$TOKEN" ]; then
+    echo "TOKEN must be defined in duck.conf"
+    exit 1
+  fi
+
+  echo url="https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&ip=" | curl -s -k -K -
+  # Append a newline to the "OK" output by curl
+  echo
+
+  sleep $INTERVAL
+done
