81ae499a732d2ef0df312d3441da8ae1ae305b73
aptalca <thedjinn@yahoo.com>
2015-04-19 12:20:11 -0400

    Clarify log message

--- firstrun.sh
+++ firstrun.sh
@@ -2,7 +2,7 @@
 
   #Search for custom config file, if it doesn't exist, copy the default one
   if [ ! -f /config/duck.conf ]; then
-    echo "Using default config file duck.conf"
+    echo "Using default config file. Please do not forget to enter your domain and token info on duck.conf"
     cp /root/duckdns/duck.conf /config/duck.conf
     chmod a+w /config/duck.conf
   fi
