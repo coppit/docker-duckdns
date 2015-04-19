e60f68b17646024e506335b724c26545e3b85712
aptalca <thedjinn@yahoo.com>
2015-04-19 11:50:51 -0400

    Add comment

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+  #Search for custom config file, if it doesn't exist, copy the default one
   if [ ! -f /config/duck.conf ]; then
+    echo "Using default config file duck.conf"
     cp /root/duckdns/duck.conf /config/duck.conf
   fi
