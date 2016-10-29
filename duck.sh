890c2f99bde1cc907f8baa8739a43715709804d4
David Becerril <david@3db.in>
2016-10-29 16:03:59 +0200

    Added ipv6 support

--- duck.sh
+++ duck.sh
@@ -32,6 +32,15 @@ if [ -z "$INTERVAL" ]; then
   INTERVAL='30m'
 fi
 
+if [ -z "$IPV6" ]; then
+  IPV6='no'
+elif [ "$IPV6" = "yes" ]; then
+  echo "Using IPV6 for updates"
+else
+  echo "For IPv6, please use IPV6=yes in duck.conf"
+  IPV6='no'
+fi
+
 if [[ ! "$INTERVAL" =~ ^[0-9]+[mhd]$ ]]; then
   echo "INTERVAL must be a number followed by m, h, or d. Example: 5m"
   exit 1
@@ -41,6 +50,9 @@ if [[ "${INTERVAL: -1}" == 'm' && "${INTERVAL:0:-1}" -lt 5 ]]; then
   echo "The shortest allowed INTERVAL is 5 minutes"
   exit 1
 fi
+
+#https://www.duckdns.org/update?domains={YOURVALUE}&token={YOURVALUE}[&ip={YOURVALUE}][&ipv6={YOURVALUE}][&verbose=true][&clear=true]
+endpoint=https://www.duckdns.org/update
 
 #-----------------------------------------------------------------------------------------------------------------------
 
@@ -52,7 +64,13 @@ function ts {
 
 while true
 do
-  RESPONSE=$(curl -S -s "https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&ip=" 2>&1)
+  if [ "$IPV6" = "yes" ]; then
+    ipaddr=`ifconfig | grep inet6 | grep -i global | awk -F " " '{print $3}'`
+  else
+    ipaddr=
+  fi
+
+  RESPONSE=$(curl -S -s "${endpoint}?domains=$DOMAINS&token=$TOKEN&ip=${ipaddr}" 2>&1)
 
   if [ "$RESPONSE" = "OK" ]
   then
